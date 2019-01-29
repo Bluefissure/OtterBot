8168d7f405f728943d68eb57a88f47b456adfae3
Bluefissure <bluefissure@foxmail.com>
2019-01-29 14:48:36 +0800

    fix trailing nerlines

--- FFXIVBOT/asgi.py
+++ FFXIVBOT/asgi.py
@@ -10,4 +10,3 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FFXIVBOT.settings")
 django.setup()
 application = get_default_application()
-
--- FFXIVBOT/routing.py
+++ FFXIVBOT/routing.py
@@ -12,4 +12,3 @@ application = ProtocolTypeRouter({
 	    ])
     )
 })
-
