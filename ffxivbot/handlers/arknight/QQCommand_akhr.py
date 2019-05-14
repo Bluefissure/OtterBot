20cb404b8aee033c7e70e0d8a489992b1142c662
Bluefissure <bluefissureofg@gmail.com>
2019-05-14 15:35:44 +0800

    add ocr to akhr

--- ffxivbot/handlers/arknight/QQCommand_akhr.py
+++ ffxivbot/handlers/arknight/QQCommand_akhr.py
@@ -12,6 +12,43 @@ import codecs
 import copy
 from bs4 import BeautifulSoup
 import traceback
+import base64
+import time
+from hashlib import sha1
+import hmac
+
+def get_image_from_CQ(CQ_text):
+    if "url=" in CQ_text:
+        tmp = CQ_text
+        tmp = tmp[tmp.find("url=") : -1]
+        tmp = tmp.replace("url=", "")
+        img_url = tmp.replace("]", "")
+        return img_url
+    return None
+
+def tencent_ocr(img_url, SecretId, SecretKey):
+    req_para = {
+        "Action":"GeneralBasicOCR",
+        "ImageUrl":img_url,
+        "Version":"2018-11-19",
+        "Region":"ap-beijing",
+        "Timestamp":int(time.time()),
+        "Nonce":random.randint(1,100000),
+        "SecretId":SecretId,
+    }
+    raw_msg = "&".join(["{}={}".format(kv[0],kv[1]) for kv in sorted(req_para.items(), key=lambda x: x[0])])
+    raw_msg = 'GETocr.tencentcloudapi.com/?'+raw_msg
+    raw = raw_msg.encode()
+    key = SecretKey.encode()
+    hashed = hmac.new(key, raw, sha1)
+    b64output = base64.encodebytes(hashed.digest()).decode('utf-8')
+    req_para.update({
+        "Signature":b64output
+    })
+    r = requests.get(url="https://ocr.tencentcloudapi.com/", params=req_para)
+    if r.status_code == 200:
+        return r.json()
+    return r.text
 
 def get_hire(akhr, tag_list):
     hr = []
@@ -67,11 +104,11 @@ def get_comb(akhr, tag_list):
 
 def get_comb_text(hr):
     msg = ""
-    max_comb = 3
+    max_comb = 5
     iter_comb = 0
     for comb in hr:
         iter_comb += 1
-        # if iter_comb > max_comb:
+        # if iter_comb > max_comb:    # limit response text length?
         #     break
         comb_name = list(comb.keys())[0]
         comb_agents = list(comb.values())[0]
@@ -102,10 +139,32 @@ def QQCommand_akhr(*args, **kwargs):
         if len(para_segs) == 0 or para_segs[0] == "help":
             msg = "/akhr $tag：按照$tag查询罗德岛公开招募（多tag空格分割）"
         else:
-            hr = get_comb(akhr, para_segs)
-            msg = get_comb_text(hr)
+            img_url = get_image_from_CQ(" ".join(para_segs))
+            msg = ""
+            if img_url:
+                SecretId = global_config["TENCENT_OCR_SECRETID"]
+                SecretKey = global_config["TENCENT_OCR_SECRETKEY"]
+                img_text = tencent_ocr(img_url, SecretId, SecretKey)
+                if not isinstance(img_text, dict):
+                    print(img_text)
+                    logging.error(img_text)
+                valid_tags = ['女性干员', '费用回复', '资深干员', '医疗干员', '减速', '位移', '特种干员', '治疗', '爆发', '男性干员', '削弱', '近战位', '先锋干员', '支援', '输出', '生存', '召唤', '控场', '防护', '高级资深干员', '群攻', '远程位', '狙击干员', '三测暂不实装', '术师干员', '近卫干员', '新手', '辅助干员', '重装干员', '快速复活']
+                tags_list = []
+                for text in img_text["Response"]["TextDetections"]:
+                    if text["DetectedText"] in valid_tags: 
+                        tags_list.append(text["DetectedText"])
+                tags_list = list(set(tags_list))
+                msg = "OCR识别结果为:{}\n========\n".format(tags_list)
+                hr = get_comb(akhr, tags_list)
+            else:
+                hr = get_comb(akhr, para_segs)
+            msg += get_comb_text(hr)
             if not msg:
-                msg = "找不到符合的结果，请检查输入参数"
+                msg += "找不到符合的结果，请检查输入参数"
+            else:
+                msg += "\nPowered by: https://bbs.nga.cn/read.php?tid=16971344"
+                if img_url:
+                    msg += " and https://cloud.tencent.com/product/ocr"
 
         msg = msg.strip()
         reply_action = reply_message_action(receive, msg)
