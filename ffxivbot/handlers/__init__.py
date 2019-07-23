5894065b475d0648bacaf0e9a08ed104e3e3211b
Bluefissure <bluefissureofg@gmail.com>
2019-07-23 13:06:56 +0800

    fix log

--- ffxivbot/handlers/__init__.py
+++ ffxivbot/handlers/__init__.py
@@ -16,7 +16,7 @@ commands = {
   "/gif":"生成沙雕图（\"/gif help\"）",
   "/dice":"DnD掷骰子（\"/dice 3d12\"）",
   "/pixiv":"Pixiv相关功能（\"/pixiv help\"）",
-  "/music":"网易云音乐搜索（\"/music 届不到的恋\"）",
+  # "/music":"网易云音乐搜索（\"/music 届不到的恋\"）",
   "/duilian":"对联（\"/duilian 稻花香里说丰年\"）",
   "/tex":"生成tex图片（\"/tex e^{\\pi i}+1=0\"）",
   "/gate":"挖宝选门（\"/gate 3\"）",
@@ -91,7 +91,7 @@ from .QQCommand_hso import QQCommand_hso
 from .QQCommand_raid import QQCommand_raid
 from .QQCommand_bot import QQCommand_bot
 from .QQCommand_pixiv import QQCommand_pixiv
-from .QQCommand_music import QQCommand_music
+# from .QQCommand_music import QQCommand_music
 from .QQCommand_duilian import QQCommand_duilian
 from .QQCommand_image import QQCommand_image
 from .QQCommand_nuannuan import QQCommand_nuannuan
