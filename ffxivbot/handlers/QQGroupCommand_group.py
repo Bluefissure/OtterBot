09379383ed16be85e2ddd0643c4371006a1281cd
Bluefissure <bluefissureofg@gmail.com>
2020-01-05 17:54:42 +0800

    update group bots limit

--- ffxivbot/handlers/QQGroupCommand_group.py
+++ ffxivbot/handlers/QQGroupCommand_group.py
@@ -19,6 +19,7 @@ def QQGroupCommand_group(*args, **kwargs):
         msg = "default msg"
         second_command_msg = receive["message"].replace("/group","",1).strip()
         second_command = second_command_msg.split(" ")[0].strip()
+        group_bots = json.loads(group.bots)
         if(user_info["role"]!="owner" and user_info["role"]!="admin"):
             msg = "仅群主与管理员有权限管理群"
         else:
@@ -34,12 +35,14 @@ def QQGroupCommand_group(*args, **kwargs):
                         "复读禁言阈值：{}\n".format(group.repeat_ban)+\
                         "投票禁言阈值：{}\n".format(group.ban_cnt)+\
                         "群成员数量：{}\n".format(len(json.loads(group.member_list)))
+                if group_bots:
+                    msg += "群机器人：{}\n".format(group_bots)
                 msg = msg.strip()
             elif(second_command=="update"):
                 msg = "群成员统计请求已发送"
                 action_list.append({
-                    "action": "get_group_member_list", 
-                    "params": {"group_id": group_id}, 
+                    "action": "get_group_member_list",
+                    "params": {"group_id": group_id},
                     "echo": "get_group_member_list:%s" % (group_id)
                 })
             elif(second_command=="api"):
@@ -50,8 +53,17 @@ def QQGroupCommand_group(*args, **kwargs):
                     group.api = False
                 group.save(update_fields=["api"])
                 msg = "群内已{}獭のAPI功能".format("开启" if group.api else "禁止")
+            elif(second_command=="bot"):
+                bot_id = second_command_msg.split(" ")[1].strip()
+                if bot_id not in group_bots:
+                    group_bots.append(bot_id)
+                else:
+                    group_bots.remove(bot_id)
+                group.bots = json.dumps(group_bots)
+                group.save(update_fields=["bots"])
+                msg = "群机器人：{}".format(group_bots)
             else:
-                msg = "错误的命令，二级命令有:\"register\", \"info\", \"api\""
+                msg = "错误的命令，二级命令有:\"register\", \"info\", \"api\", \"bot\""
         reply_action = reply_message_action(receive, msg)
         action_list.append(reply_action)
         return action_list
