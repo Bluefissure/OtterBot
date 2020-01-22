b5b88fd67eb19e2b8b0fcaec9c1b539c49d2fac9
Bluefissure <bluefissureofg@gmail.com>
2020-01-23 03:45:36 +0800

    fix

--- FFXIV/settings_example.py
+++ FFXIV/settings_example.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import re
-import pymysql
-pymysql.version_info = (1, 3, 13, "final", 0)
-pymysql.install_as_MySQLdb()
+#import pymysql
+#pymysql.version_info = (1, 3, 13, "final", 0)
+#pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
