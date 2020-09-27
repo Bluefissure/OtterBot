302c3883e3d2194d2ce70da15c2f512feb640b63
Bluefissure <bluefissureofg@gmail.com>
2020-09-27 08:21:09 +0800

    fix /hhsh

--- ffxivbot/handlers/QQCommand_hhsh.py
+++ ffxivbot/handlers/QQCommand_hhsh.py
@@ -19,17 +19,17 @@ def QQCommand_hhsh(*args, **kwargs):
         user = QQUser.objects.get(user_id=receive["user_id"])
         content = receive["message"].replace("/hhsh", "", 1).strip()
         api_url = "https://lab.magiconch.com/api/nbnhhsh/"
-        r = requests.post(api_url + "guess", data={"text": content})
+        r = requests.post(api_url + "guess", data={"text": content}, timeout=3)
         if r.status_code != 200:
             msg = "API Error: HTTP {}".format(r.status_code)
         else:
             j = r.json()
-            if not j:
-                msg = "未能查询到所包含缩写内容\n请前往 https://lab.magiconch.com/nbnhhsh/ 贡献词条"
-            else:
-                msg = ""
-                for item in j:
+            msg = ""
+            for item in j:
+                if "name" in item and "trans" in item:
                     msg += "{}: {}\n".format(item["name"], " ".join(item["trans"]))
+            if not msg:
+                msg = "未能查询到所包含缩写内容\n请前往 https://lab.magiconch.com/nbnhhsh/ 贡献词条"
         reply_action = reply_message_action(receive, msg)
         action_list.append(reply_action)
         return action_list
