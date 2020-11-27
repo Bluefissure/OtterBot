230b4d98269305b0f284e0076f7c34b5663b64ea
Bluefissure <bluefissure@foxmail.com>
2020-11-28 05:54:11 +0800

    Support more clients

--- ffxivbot/views/tata.py
+++ ffxivbot/views/tata.py
@@ -18,49 +18,46 @@ def generate_bot_conf(bot, client, web_base, http_url, ws_url):
     bot_conf = {"error": "Unsupported client."}
     if client == "Mirai":
         bot_conf = {
-            "proxy":"" ,
+            "proxy": "",
             "bots": {
-              str(bot.user_id): {
-                "cacheImage": True,
-                "cacheRecord": True,
-                "heartbeat": {
-                    "enable": True,
-                    "interval": 15000,
-                },
-                "http": {
-                    "enable": False,
-                    "host": "0.0.0.0",
-                    "port": 5700,
-                    "accessToken": "",
-                    "postUrl": "",
-                    "postMessageFormat": "string",
-                    "secret": "",
-                },
-                "ws_reverse": [
-                    {
-                        "enable": True,
-                        "postMessageFormat": "string",
-                        "reverseHost": web_base,
-                        "reversePort": 80,
+                str(bot.user_id): {
+                    "cacheImage": True,
+                    "cacheRecord": True,
+                    "heartbeat": {"enable": True, "interval": 15000,},
+                    "http": {
+                        "enable": False,
+                        "host": "0.0.0.0",
+                        "port": 5700,
                         "accessToken": "",
-                        "reversePath": "/ws",
-                        "reverseApiPath": "/api",
-                        "reverseEventPath": "/event",
-                        "useUniversal": True,
-                        "useTLS": False,
-                        "reconnectInterval": 3000,
-                    }
-                ],
-                "ws": {
-                    "enable": False,
-                    "postMessageFormat": "string",
-                    "accessToken": "SECRET",
-                    "wsHost": "0.0.0.0",
-                    "wsPort": 8080,
+                        "postUrl": "",
+                        "postMessageFormat": "string",
+                        "secret": "",
+                    },
+                    "ws_reverse": [
+                        {
+                            "enable": True,
+                            "postMessageFormat": "string",
+                            "reverseHost": web_base,
+                            "reversePort": 80,
+                            "accessToken": "",
+                            "reversePath": "/ws",
+                            "reverseApiPath": "/api",
+                            "reverseEventPath": "/event",
+                            "useUniversal": True,
+                            "useTLS": False,
+                            "reconnectInterval": 3000,
+                        }
+                    ],
+                    "ws": {
+                        "enable": False,
+                        "postMessageFormat": "string",
+                        "accessToken": "SECRET",
+                        "wsHost": "0.0.0.0",
+                        "wsPort": 8080,
+                    },
                 },
             },
-        },
-    }
+        }
         if bot.api_post_url:
             bot_conf["bots"][str(bot.user_id)]["http"]["enable"] = True
             bot_conf["bots"][str(bot.user_id)]["http"]["postUrl"] = http_url
@@ -75,61 +72,59 @@ def generate_bot_conf(bot, client, web_base, http_url, ws_url):
     elif client == "YaYa":
         bot_conf = {
             "version": "1.0.5",
-            "master":  12345678,
-            "debug":   True,
+            "master": 12345678,
+            "debug": True,
             "cache": {
                 "database": False,
-                "image":    False,
-                "record":   False,
-                "video":    False,
+                "image": False,
+                "record": False,
+                "video": False,
             },
-            "heratbeat": {
-                "enable":   True,
-                "interval": 10000,
-            },
+            "heratbeat": {"enable": True, "interval": 10000,},
             "bots": [
                 {
-                "bot": 0,
-                "websocket": [
-                    {
-                        "name": "WSS EXAMPLE",
-                        "enable": False,
-                        "host": "127.0.0.1",
-                        "port": 6700,
-                        "access_token": "",
-                        "post_message_format": "string",
-                    },
-                ],
-                "websocket_reverse": [
-                    {
-                        "name": "WSC EXAMPLE",
-                        "enable": True,
-                        "url": "ws://127.0.0.1:8080/ws",
-                        "api_url": "ws://127.0.0.1:8080/api",
-                        "event_url": "ws://127.0.0.1:8080/event",
-                        "use_universal_client": True,
-                        "access_token": "",
-                        "post_message_format": "string",
-                        "reconnect_interval": 3000,
-                    },
-                ],
-                "http": [
-                    {
-                        "name": "HTTP EXAMPLE",
-                        "enable": False,
-                        "host": "127.0.0.1",
-                        "port": 5700,
-                        "access_token": "",
-                        "post_url": "http://127.0.0.1/plugin",
-                        "secret": "",
-                        "time_out": 0,
-                        "post_message_format": "string",
-                    },
-                ],
+                    "bot": 0,
+                    "websocket": [
+                        {
+                            "name": "WSS EXAMPLE",
+                            "enable": False,
+                            "host": "127.0.0.1",
+                            "port": 6700,
+                            "access_token": "",
+                            "post_message_format": "string",
+                        },
+                    ],
+                    "websocket_reverse": [
+                        {
+                            "name": "WSC EXAMPLE",
+                            "enable": True,
+                            "url": "ws://127.0.0.1:8080/ws",
+                            "api_url": "ws://127.0.0.1:8080/api",
+                            "event_url": "ws://127.0.0.1:8080/event",
+                            "use_universal_client": True,
+                            "access_token": "",
+                            "post_message_format": "string",
+                            "reconnect_interval": 3000,
+                        },
+                    ],
+                    "http": [
+                        {
+                            "name": "HTTP EXAMPLE",
+                            "enable": False,
+                            "host": "127.0.0.1",
+                            "port": 5700,
+                            "access_token": "",
+                            "post_url": "http://127.0.0.1/plugin",
+                            "secret": "",
+                            "time_out": 0,
+                            "post_message_format": "string",
+                        },
+                    ],
                 },
             ],
         }
         bot_conf["bots"][0]["bot"] = bot.user_id
+        bot_conf["master"] = int(bot.owner_id)
         if bot.api_post_url:
             bot_conf["bots"][0]["http"][0]["name"] = "tata"
             bot_conf["bots"][0]["http"][0]["enable"] = True
@@ -139,9 +134,79 @@ def generate_bot_conf(bot, client, web_base, http_url, ws_url):
         else:
             bot_conf["bots"][0]["websocket_reverse"][0]["name"] = "tata"
             bot_conf["bots"][0]["websocket_reverse"][0]["enable"] = True
-            bot_conf["bots"][0]["websocket_reverse"][0]["url"] = f'ws://{web_base}:80/ws'
-            bot_conf["bots"][0]["websocket_reverse"][0]["access_token"] = bot.access_token
+            bot_conf["bots"][0]["websocket_reverse"][0][
+                "url"
+            ] = f"ws://{web_base}:80/ws"
+            bot_conf["bots"][0]["websocket_reverse"][0][
+                "access_token"
+            ] = bot.access_token
         bot_conf = yaml.dump(bot_conf).encode()
+    elif client == "Go-cqhttp":
+        if bot.api_post_url:
+            use_http = "true"
+            use_ws = "false"
+            post_url = '"{}"'.format(http_url)
+            ws_reverse_url = ""
+        else:
+            use_http = "false"
+            use_ws = "true"
+            post_url = ""
+            ws_reverse_url = '"{}"'.format(ws_url)
+        conf = (bot.user_id, bot.access_token, use_http, post_url, use_ws, ws_reverse_url)
+        bot_conf = """{{
+    uin: {}
+    password: ""
+    encrypt_password: false
+    password_encrypted: ""
+    enable_db: true
+    access_token: "{}"
+    relogin: {{
+        enabled: true
+        relogin_delay: 3
+        max_relogin_times: 0
+    }}
+    _rate_limit: {{
+        enabled: false
+        frequency: 1
+        bucket_size: 1
+    }}
+    ignore_invalid_cqcode: false
+    force_fragmented: false
+    heartbeat_interval: 5
+    http_config: {{
+        enabled: {}
+        host: 0.0.0.0
+        port: 5700
+        timeout: 0
+        post_urls: {}
+    }}
+    ws_config: {{
+        enabled: false
+        host: 0.0.0.0
+        port: 6700
+    }}
+    ws_reverse_servers: [
+        {{
+            enabled: {}
+            reverse_url: {}
+            reverse_api_url: ""
+            reverse_event_url: ""
+            reverse_reconnect_interval: 3000
+        }}
+    ]
+    post_message_format: string
+    use_sso_address: false
+    debug: false
+    log_level: ""
+    web_ui: {{
+        enabled: true
+        host: 127.0.0.1
+        web_ui_port: 9999
+        web_input: false
+    }}
+}}""".format(
+            *conf
+        )
     elif client == "OICQ":
         access_token = '"{}"'.format(bot.access_token)
         if bot.api_post_url:
@@ -154,7 +219,7 @@ def generate_bot_conf(bot, client, web_base, http_url, ws_url):
             ws_reverse_url = '"{}",'.format(ws_url)
         conf = (use_http, access_token, access_token, post_url, ws_reverse_url)
         # print(conf)
-        bot_conf = bot_conf = """"use strict";
+        bot_conf = """"use strict";
 module.exports = {{
     general: {{
         platform:           2,
