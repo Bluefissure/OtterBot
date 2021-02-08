a5ca16aff9a5fe6213d12868316de94df8c8d12a
雪狐 <mengshouer@outlook.com>
2021-02-08 05:31:45 +0000

    Update QQCommand_cat.py

--- ffxivbot/handlers/QQCommand_cat.py
+++ ffxivbot/handlers/QQCommand_cat.py
@@ -4,6 +4,7 @@ from ffxivbot.models import *
 import logging
 import json
 import random
+import requests
 
 
 def QQCommand_cat(*args, **kwargs):
@@ -11,7 +12,15 @@ def QQCommand_cat(*args, **kwargs):
         QQ_BASE_URL = kwargs["global_config"]["QQ_BASE_URL"]
         action_list = []
         receive = kwargs["receive"]
-        img_url = QQ_BASE_URL + "static/cat/%s.jpg" % (random.randint(0, 750))
+        r = random.randint(1,6)
+        if r <= 3:
+            img_url = QQ_BASE_URL + "static/cat/%s.jpg" % (random.randint(0, 750))
+        else:
+            try:
+                api_url = "https://api.thecatapi.com/v1/images/search"
+                img_url = requests.get(api_url).json()[0]["url"]
+            except:
+                img_url = QQ_BASE_URL + "static/cat/%s.jpg" % (random.randint(0, 750))
         msg = "[CQ:image,file={}]".format(img_url)
         msg = [{"type": "image", "data": {"file": img_url},}]
         reply_action = reply_message_action(receive, msg)
