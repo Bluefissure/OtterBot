42e5c0e4f52b80a87cd68abd1818aac4ab2de2f1
a08381 <632785425@163.com>
2022-04-27 12:33:15 +0800

    Update hunt_maintain.py

--- utils/hunt_maintain.py
+++ utils/hunt_maintain.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import time
+import datetime
 import django
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -12,7 +13,7 @@ django.setup()
 from ffxivbot.models import *
 
 
-def hunt_maintain(keyword: str = '国', maintain_time = time.time()):
+def hunt_maintain(keyword: str = '国', strtime: str = ''):
     if keyword == "国服" or keyword == '国':
         servers = Server.objects.all()
     elif keyword == "陆行鸟" or keyword == '鸟':
@@ -23,7 +24,13 @@ def hunt_maintain(keyword: str = '国', maintain_time = time.time()):
         servers = Server.objects.filter(areaId=7)
     elif keyword == "豆豆柴" or keyword == '狗':
         servers = Server.objects.filter(areaId=8)
-    servers = Server.objects.filter(name=keyword)
+    else:
+        servers = Server.objects.filter(name=keyword)
+
+    if strtime != '':
+        maintain_time = datetime.datetime.strptime(strtime + '+0800', '%Y%m%d_%H%M%S%z').timestamp()
+    else:
+        maintain_time = time.time()
     for s in servers:
         hunt_log = HuntLog(server=s, log_type="maintain", time=maintain_time)
         hunt_log.save()
