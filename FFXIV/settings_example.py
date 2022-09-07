41eaab700573a0bbebae87dd9dd5752fd8fe0baf
Bluefissure <bluefissure@foxmail.com>
2022-09-08 00:38:04 +0800

    Upgrade DJango to 4.1 & add CONN_HEALTH_CHECKS

--- FFXIV/settings_example.py
+++ FFXIV/settings_example.py
@@ -132,6 +132,7 @@ DATABASES = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+CONN_HEALTH_CHECKS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
