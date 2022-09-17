b7c83cc0528c827c66d4eecb2c7305f324b2ea9d
Bluefissure <Bluefissure@users.noreply.github.com>
2022-09-17 11:12:27 -0500

    Revert "nuannuan use official api to get data (#138)"
    This reverts commit cf6bdca1ef3dfde6fdd2cfa6d6ebb9b6dc40e6d4.

--- ffxivbot/handlers/QQCommand_nuannuan.py
+++ ffxivbot/handlers/QQCommand_nuannuan.py
@@ -1,41 +1,46 @@
 from .QQEventHandler import QQEventHandler
 from .QQUtils import *
+from .RsshubUtil import RsshubUtil
 from ffxivbot.models import *
 import logging
+import json
+import random
 import requests
 import re
 import traceback
+from bs4 import BeautifulSoup
 
-def get_video_id(mid):
+def extract_url(text: str):
     try:
-        # 获取用户信息最新视频的前五个，避免第一个视频不是攻略ps=5处修改
-        url = f"https://api.bilibili.com/x/space/arc/search?mid={mid}&order=pubdate&pn=1&ps=5"
-        r = requests.get(url, timeout=3).json()
-        video_list = r["data"]["list"]["vlist"]
-        for i in video_list:
-            if re.match(r"【FF14\/时尚品鉴】第\d+期 满分攻略", i["title"]):
-                return i["bvid"]
+        aid = re.compile(r'(av|AV)\d+').search(text)
+        bvid = re.compile(r'(BV|bv)([a-zA-Z0-9])+').search(text)
+        if bvid:
+            url = f'https://api.bilibili.com/x/web-interface/view?bvid={bvid[0]}'
+        else:
+            url = f'https://api.bilibili.com/x/web-interface/view?aid={aid[0][2:]}'
+        return url
     except:
         traceback.print_exc()
     return None
 
-def extract_nn(bvid):
+def extract_nn(feed):
     try:
-        url = f"https://api.bilibili.com/x/web-interface/view?bvid={bvid}"
-        r = requests.get(url, timeout=3).json()
-        if r["code"] == 0:
-            url = f"https://www.bilibili.com/video/{bvid}"
-            title = r["data"]["title"]
-            desc = r["data"]["desc"]
-            text = desc.replace("个人攻略网站", "游玩C攻略站")
-            image = r["data"]["pic"]
-            res_data = {
-                "url": url,
-                "title": title,
-                "content": text,
-                "image": image,
-            }
-            return res_data
+        pattern = r"【FF14\/时尚品鉴】第\d+期 满分攻略"
+        for item in feed["items"]:
+            if re.match(pattern, item["title"]):
+                h = BeautifulSoup(item["summary"], "lxml")
+                image = re.sub('https://www.bilibili.com/video/', '', h.img.attrs["src"])
+                api_url = extract_url(item["link"])
+                r = requests.get(api_url, timeout=3).json()
+                text = r['data']['desc']
+                text = text.replace("个人攻略网站", "游玩C攻略站")
+                res_data = {
+                    "url": item["id"],
+                    "title": item["title"],
+                    "content": text,
+                    "image": image,
+                }
+                return res_data
     except:
         traceback.print_exc()
     return None
@@ -46,10 +51,13 @@ def QQCommand_nuannuan(*args, **kwargs):
     try:
         receive = kwargs["receive"]
         try:
-            # 获取视频av号(aid)
-            bvid = get_video_id(15503317)
-            # 获取数据
-            res_data = extract_nn(bvid)
+            rsshub = RsshubUtil()
+            feed = rsshub.biliuservedio(15503317)
+            res_data = extract_nn(feed)
+            # print("res_data:{}".format(res_data))
+            if not res_data:
+                feed = rsshub.biliuserdynamic(15503317)
+                res_data = extract_nn(feed)
             if not res_data:
                 msg = "无法查询到有效数据，请稍后再试"
             else:
